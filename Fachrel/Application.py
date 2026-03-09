4c94cfad982232ab938b059941528a0947dabd21
Chapler <fachrelfayaaz23@gmail.com>
2026-03-09 12:26:35 +0000

    Tolong ini yang baca jadiin ke java dong plis ganteng

--- Fachrel/Application.py
+++ Fachrel/Application.py
@@ -2,10 +2,10 @@ from demo import demolinkedlist
 from element import Element
 
 test = demolinkedlist()
-test.append('asep')
-test.append('budi')
-test.append('joni')
-test.append('bobby')
-test.append('dadang')
-test.append('teddy')
+test.append('asep =>')
+test.append('budi =>') 
+test.append('joni =>')
+test.append('bobby =>')
+test.append('dadang =>')
+test.append('teddy =>')
 test.ShowAll()
